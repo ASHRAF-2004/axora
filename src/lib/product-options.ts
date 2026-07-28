6f52cf5b88f988b34f2308edd8515f889926ad39
ASHRAF <adoashraf103@gmail.com>
2026-07-28 11:32:24 +0800

    Centralize product form options

---
+++ src/lib/product-options.ts
@@ -0,0 +1,18 @@
+export const PRODUCT_CATEGORIES = [
+  "Office Basics",
+  "Pantry / Hospitality",
+  "Cleaning & Hygiene",
+  "Printing & Branding / Marketing",
+] as const;
+
+export const PRODUCT_UNITS = [
+  "Piece",
+  "Pack",
+  "Box",
+  "Bottle",
+  "Roll",
+  "Carton",
+  "Ream",
+  "Jar",
+  "Sheet",
+] as const;
